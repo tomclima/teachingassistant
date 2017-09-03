47a6be79ea7c35d2f1d15ebb913cf56e91e8fca8
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:39:08 -0300

    serviço de aluno injetado, não criado pelo componente

--- gui/ta-gui/src/app/app.component.ts
+++ gui/ta-gui/src/app/app.component.ts
@@ -11,8 +11,9 @@ import { AlunoService } from './aluno.service';
   standalone: false
 })
 export class AppComponent {
+   constructor(private alunoService: AlunoService) {}
    aluno: Aluno = {nome: "", cpf: "", email: "", github:""};
-   alunoService = new AlunoService();
+
    alunos: Aluno[] = [];
    cpfduplicado: boolean = false;
 

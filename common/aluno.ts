1ab9bb97b4105892b5b9da8e55d5d0aeca7e09b9
pauloborba <phmb@cin.ufpe.br>
2017-09-12 20:51:20 -0300

    servidor integrado ao cliente

--- common/aluno.ts
+++ common/aluno.ts
@@ -17,7 +17,6 @@ export class Aluno {
 
   clone(): Aluno {
     var aluno: Aluno = new Aluno();
-    aluno.metas = new Map<string,string>();
     aluno.copyFrom(this);
     return aluno;
   }
@@ -32,7 +31,7 @@ export class Aluno {
   copyMetasFrom(from: Map<string,string>): void {
     this.metas = new Map<string,string>();
     for (let key in from) {
-      this.metas.set(key, this.metas.get(key));
+      this.metas.set(key, this.metas.get(key)!);
     }
   }
 }

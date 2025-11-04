2f276df3f180a338210837458dfb0a1790449400
tomclima <tomecostalima1228@gmail.com>
2025-11-04 00:30:10 -0300

    correction to evitar cadastro

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -3,11 +3,11 @@ import { Aluno } from './aluno';
 export class AlunoService {
   alunos: Aluno[] = [];
   
-  gravar(aluno: Aluno): Aluno {
+  gravar(aluno: Aluno): Aluno | null {
     var result = null;
     if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
       this.alunos.push(aluno);
-      result = aluno;
+      return aluno;
     }
     return result;
   }
